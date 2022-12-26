d4aa5452a712496c4c9c867021f31bb2fee091ac
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2022-12-26 14:35:01 +0100

    Update ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import warning from './warning.svg';
-import './fonts.css';
-import './App.css';
+import warning from './warning.svg'
+import './fonts.css'
+import './App.css'
 
 function App() {
+  if (!document.referrer || document.referrer === window.location.href) {
+    window.location.href = 'https://station.terra.money'
+  }
   return (
-    <div className="App">
+    <div className='App'>
       <img src={warning} alt='warning' />
-      <h2>SCAM ALERT</h2>
+      <h2>PHISHING ALERT</h2>
       <p>
-        You are trying to visit <span className='url'>{document.referrer}</span><br/>which is a known scam website.
+        You are trying to visit <span className='url'>{document.referrer}</span>
+        <br />
+        which is a known scam website.
       </p>
       <span
         onClick={() => {
-          window.location.href = document.referrer;
+          window.location.href = document.referrer
         }}
         className='button'
       >
         Visit anyway
       </span>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
